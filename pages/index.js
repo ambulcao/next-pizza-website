bacf861ea242b0f71276012d1cedc42c4da43e4e
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-05-24 14:27:56 +0000

    Files Website

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+      </Head>
+
+      <main>
+        <h1>Pizzzza</h1>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
